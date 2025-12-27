53d07b4ea538a4c2c2b81865cbad3b5bcb1e9dbf
rahmed-dev <ra9496300@gmail.com>
2025-12-27 16:54:49 +0500

    add missing child Table

---
+++ gvm_payroll/gvm_payroll/doctype/internal_salary_details/internal_salary_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Samuael Ketema and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class InternalSalaryDetails(Document):
+	pass
